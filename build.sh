8254629eb007e79ede67f7e2930dadd43e5568e6
Pankaj Doharey <pankajdoharey@live.com>
2017-05-10 14:17:10 +0530

    Updates build script

--- build.sh
+++ build.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-echo "Removing Build Directory\n"
+echo "Removing Build Directory."
 rm -rf build
-echo "Pushing existing development branch commit ...\n"
+echo "Pushing existing development branch commit ..."
 git push origin development
-echo "Middleman Build ...\n"
+echo "Middleman Build ..."
 middleman build
-echo "Copy CNAME to build/ ...\n"
+echo "Copy CNAME to build/ ..."
 cp CNAME build/
 cd build/
-echo "Initialising Git ...\n"
+echo "Initialising Git ..."
 git init
 git remote add origin "https://github.com/codemismatch/codemismatch.github.io.git"
-echo "Fetch --- \n"
+echo "Fetch --- "
 git fetch --depth 0
 git checkout --orphan master
 git add .
-echo "Automated Commit ..\n"
+echo "Automated Commit .."
 git commit -m "Automated Deploy commit by build script $(date)"
 git push origin master --force
